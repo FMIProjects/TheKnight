cfc5fd357a2454beabbe4c39e957545b52e125cb
GabroveanuRazvan <gabroveanu@gmail.com>
2024-03-16 13:02:08 +0200

    Added camera bound.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -7,7 +7,8 @@ public class CameraMovement : MonoBehaviour
 
     public Transform target;
     public float smoothing;
-
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +23,12 @@ public class CameraMovement : MonoBehaviour
         {   
             // target that keeps the camera in place on the z axis so thatit does not go out of bounds
             Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+
+            // camera bounding part
+
+            targetPosition.x =Mathf.Clamp(targetPosition.x, minPosition.x,maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
 
             // interpolates between current object 
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
