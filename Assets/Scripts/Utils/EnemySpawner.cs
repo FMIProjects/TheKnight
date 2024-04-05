d66a28cb79a60ece6e00a677255f3d1c2a40b72a
GabroveanuRazvan <gabroveanu@gmail.com>
2024-04-05 14:36:53 +0300

    Added enemy spawner feature. Removed random collision objects.

--- Assets/Scripts/Utils/EnemySpawner.cs
+++ Assets/Scripts/Utils/EnemySpawner.cs
@@ -9,19 +9,21 @@ public class EnemySpawner : MonoBehaviour
     public Vector2 minSpawnCoordinates;
     public Vector2 maxSpawnCoordinates;
     public float spawnInterval = 10f;
-
+    public bool isActive = true;
 
     // Start is called before the first frame update
     void Start()
-    {
-        StartCoroutine(SpawnEnemiesRoutine());
+    { 
+           StartCoroutine(SpawnEnemiesRoutine());
     }
 
     IEnumerator SpawnEnemiesRoutine()
     {
         while(true)
         {
-            SpawnEnemies();
+            // spawn while active
+            if (isActive)
+                SpawnEnemies();
 
             yield return new WaitForSeconds(spawnInterval);
         }
@@ -32,10 +34,11 @@ public class EnemySpawner : MonoBehaviour
 
         for(int i = 0;i<3;i++)
         {
+            // generate a random position in the given area 
             Vector2 spawnPosition = new Vector2(Random.Range(minSpawnCoordinates.x, maxSpawnCoordinates.x),
                                            Random.Range(minSpawnCoordinates.y, maxSpawnCoordinates.y));
 
-          
+            // instantiate a new enemies based on a prefab
             Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
         }
 
