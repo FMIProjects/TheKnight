33f9be92da5753bd074a9e4f4dc9b53fb4d5375b
GabroveanuRazvan <gabroveanu@gmail.com>
2024-05-17 18:16:24 +0300

    Added Scene Switching Mechanic.

---
+++ Assets/Scripts/ProcedualGeneration/SceneSwitching.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneName; // Numele scenei în care va fi teleportat player-ul
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
